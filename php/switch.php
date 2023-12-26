4a53bf4684bb0d53577c932c9d8bdd02cd238aa2
Rafiea-Ashraf <rafiea.ashraf@gmail.com>
2023-12-26 20:06:31 +0500

    hello

---
+++ php/switch.php
@@ -0,0 +1,24 @@
+<?php
+	$number = 4;
+	
+	switch($number){
+	  case '1':
+	  echo 'Number is one';
+	  break;
+	  case '2':
+	  echo 'Number is two';
+	  break;
+	  case '3':
+	  echo 'Number is three';
+	  break;
+	  case '4':
+	  echo 'Number is four';
+	  break;
+	  case '5':
+	  echo 'Number is five';
+	  break;
+	  case '6':
+	  echo 'Number is six';
+	  break;
+	} 
+?>
